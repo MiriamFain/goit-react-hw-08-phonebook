71ee166aa5d2b7b9396748a798f9116f59eb231e
Мириам Файнштейн <MIRIAM.FAINSHTEIN@gmail.com>
2022-11-18 15:33:20 +0200

    Create Selector

---
+++ src/store/selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectAllContacts = state => state.contacts.items;
+
+export const selectFilter = state => state.filter;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+  [selectFilter, selectAllContacts],
+  (filter, contacts) => {
+    const optimizedFilter = filter.toLowerCase();
+    return contacts.length
+      ? contacts.filter(({ name }) =>
+          name.toLowerCase().includes(optimizedFilter)
+        )
+      : [];
+  }
+);
