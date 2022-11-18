6e179c49a73d83c2d8980e599418e881bfd954f9
Мириам Файнштейн <MIRIAM.FAINSHTEIN@gmail.com>
2022-11-18 14:44:50 +0200

    Remove unused files

--- src/store/actions/filterActions.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-const changeFilter = createAction('filter/changeFilter');
-export default changeFilter;
--- src/store/reducers/filterReducer.js
+++
@@ -1,6 +0,0 @@
-import { createReducer } from '@reduxjs/toolkit';
-import changeFilter from '../actions/filterActions';
-
-export default createReducer('', {
-  [changeFilter]: (state, action) => action.payload,
-});
