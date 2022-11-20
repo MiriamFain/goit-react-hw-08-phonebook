26a394006487098f77c3110fc7954145e1670a7d
Мириам Файнштейн <MIRIAM.FAINSHTEIN@gmail.com>
2022-11-20 17:40:08 +0200

    Add Shared layout

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,47 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import s from './SharedLayout.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUserThunk } from 'store/auth/thunk.auth';
+// import s from './user-menu.module.css';
+import { BiExit } from 'react-icons/bi';
+import { selectUser } from 'store/selectors';
+import { Link } from 'react-router-dom';
+// import { useMediaQuery } from 'react-responsive';
+
+const SharedLayout = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const userName =
+    user && user.name[0].toUpperCase() + user.name.slice(1).toLowerCase();
+  //   const isMobile = useMediaQuery({
+  //     query: '(max-width: 600px)',
+  //   });
+  const handleExit = () => {
+    dispatch(logoutUserThunk());
+  };
+  return (
+    <>
+      <header className={s.bar}>
+        <Link to="/" className={s.logo}>
+          'Phonebook'
+        </Link>
+        <p className={s.user}>
+          Welcome <span className={s.name}> {userName}</span>
+        </p>
+        <button type="button" onClick={handleExit} className={s.exit}>
+          <BiExit className={s.exitIcon} /> Exit
+        </button>
+      </header>
+      <div className={s.layout}>
+        <main className={s.main}>
+          <Suspense fallback="Wait a moment....">
+            <Outlet />
+          </Suspense>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default SharedLayout;
